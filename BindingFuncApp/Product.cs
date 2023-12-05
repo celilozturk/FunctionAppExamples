daf2a71a8e98bdf352d8b83c61bb8cc5e6964a12
Celil Ozturk <celil.ozturk@windowslive.com>
2023-12-05 23:28:36 +0300

    Table output binding example1

---
+++ BindingFuncApp/Product.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingFuncApp
+{
+    public class Product:TableEntity
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Color { get; set; }
+    }
+}
