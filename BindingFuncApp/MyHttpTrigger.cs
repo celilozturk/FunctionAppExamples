5ebb20e4631f56cdd4264e8ae10eb9f13d5f9865
Celil Ozturk <celil.ozturk@windowslive.com>
2023-12-06 19:41:25 +0300

    return code refactor

--- BindingFuncApp/MyHttpTrigger.cs
+++ BindingFuncApp/MyHttpTrigger.cs
@@ -17,25 +17,22 @@ namespace BindingFuncApp
     public static class MyHttpTrigger
     {
         [FunctionName("MyHttpTrigger")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log, [Table("Products",Connection="MyAzureStorage")] CloudTable cloudTable, [Queue("queueproduct",Connection ="MyAzureStorage")] CloudQueue cloudQueue) //Output Binding islemi
-        {
-           
+        [return:Table("Products", Connection = "MyAzureStorage")]
+        public static async Task<Product> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log, [Queue("queueproduct",Connection ="MyAzureStorage")] CloudQueue cloudQueue) //Output Binding islemi
+        {          
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             Product newProduct = JsonConvert.DeserializeObject<Product>(requestBody);
 
-            TableOperation operation = TableOperation.Insert(newProduct);
-
-            await cloudTable.ExecuteAsync(operation);
 
             var productString=JsonConvert.SerializeObject(newProduct);  
             CloudQueueMessage cloudQueueMessage = new CloudQueueMessage(productString);
             await cloudQueue.AddMessageAsync(cloudQueueMessage);
-               
 
-            return new OkObjectResult(newProduct);
+
+            return newProduct;
         }
     }
 }
