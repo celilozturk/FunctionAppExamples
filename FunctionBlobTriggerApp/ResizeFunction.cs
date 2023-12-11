25a5ac56faf5080bd910aa97fe3970395080cdff
Celil Ozturk <celil.ozturk@windowslive.com>
2023-12-11 17:52:13 +0300

    resize2

--- FunctionBlobTriggerApp/ResizeFunction.cs
+++ FunctionBlobTriggerApp/ResizeFunction.cs
@@ -1,9 +1,11 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Processing;
 
@@ -12,18 +14,28 @@ namespace FunctionBlobTriggerApp
     public class ResizeFunction
     {
         [FunctionName("ResizeFunction")]
-        public async Task Run([BlobTrigger("udemy-pictures/{name}", Connection = "MyAzureStorage")]Stream myBlob, string name, ILogger log, [Blob("udemy-pictures-resize/{name}",FileAccess.Write,Connection ="MyAzureStorage")] Stream outputBlobStream)
+        public async Task Run([BlobTrigger("udemy-pictures/{name}", Connection = "MyAzureStorage")]Stream myBlob, string name, ILogger log, [Blob("udemy-pictures-resize",Connection ="MyAzureStorage")] CloudBlobContainer cloudBlobContainer)
         {
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
-
-
+            await cloudBlobContainer.CreateIfNotExistsAsync();
+            MemoryStream ms = new MemoryStream();
 
             var format=await Image.DetectFormatAsync(myBlob);
+            var blockBlob=cloudBlobContainer.GetBlockBlobReference($"{Guid.NewGuid()}-100.{format.FileExtensions.First()}");
+           // var blockBlob=cloudBlobContainer.GetBlockBlobReference($"{Guid.NewGuid()}-200.{format.FileExtensions.First()}");
+
 
             var ResizeImage = Image.Load(myBlob);
             
             ResizeImage.Mutate(x=>x.Resize(100,100));
-            ResizeImage.Save(outputBlobStream, format);
+           
+
+            ResizeImage.Save(ms, format);
+
+            ms.Position = 0;
+            blockBlob.UploadFromStreamAsync(ms);
+            ms.Close();
+            ms.Dispose();
+
             log.LogInformation($"Resim resize islemi basaryla gerceklestirildi!");
 
         }
